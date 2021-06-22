f0b3451daecf328a97c9a501635a04fbf1f352a1
Phil Varner <philvarner@gmail.com>
2021-06-22 13:38:59 -0400

    don't destroy, and empty params

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,3 @@
 export AWS_ACCOUNT_ID=$(aws sts get-caller-identity | jq .Account -r)
 export AWS_REGION=$(aws configure get region)
 cdk deploy -vvv --all --require-approval never
-cdk destroy -f -vvv --all --require-approval never
