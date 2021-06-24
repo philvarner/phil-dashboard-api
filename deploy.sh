312c3f6acc1e84ba72cd8bb5b41cfcfa70a8abe8
Phil Varner <philvarner@gmail.com>
2021-06-24 14:09:16 -0400

    login to ecr

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 export AWS_ACCOUNT_ID=$(aws sts get-caller-identity | jq .Account -r)
 export AWS_REGION=$(aws configure get region)
+export REG=${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com
+
+aws ecr get-login-password --region ${AWS_REGION} | docker login --password-stdin --username AWS ${REG}
+
 cdk deploy -vvv --all --require-approval never
